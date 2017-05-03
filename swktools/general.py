a2cb44e7f30948f8be2ad696e10cf9215dc04f4e
swkeemink <swkeemink@scimail.eu>
2017-05-03 17:50:16 +0100

    ENH: added spike matching function

--- swktools/general.py
+++ swktools/general.py
@@ -8,6 +8,73 @@ from skimage.feature import register_translation as rt
 from scipy.ndimage.fourier import fourier_shift
 
 
+def spike_match(real, est, maxdelay ):
+    """Matches real spiketimes with estimated spiketimes.
+
+    Parameters
+    ----------
+    real : array
+      real spike times
+    estimated : array
+      estimated spike times
+    maxdelay : float
+      maximum delay for matching spiketimes
+
+    Returns
+    -------
+    float
+      ER score (1 - F1 = 1- true_p/(true_p+false_p+false_n) )
+    int
+      True positives
+    int
+      False positives
+    int
+      False negatives
+    array
+      Labels on real times (true_p: 1, false_n: 2)
+    array
+      Labels on estimated times (true_p: 1, false_p: 3)
+      """
+    # start counters
+    true_p = 0
+    false_n = 0
+
+    # start labels
+    real_labels = np.zeros(len(real))
+    est_labels = np.zeros(len(est))
+
+    # loop over all real spikes
+    for i in range(len(real)):
+        # get current time
+        cur_time = real[i]
+
+        # get differences with current estimated times
+        diffs = abs(cur_time - est)
+
+        # find all match ids
+        ids = np.nonzero(np.logical_and(diffs <= maxdelay, est_labels == 0))
+        ids = np.array(ids)[0]
+
+        # if any match, remember the first as true positive
+        if ids.size != 0:
+            est_labels[ids[0]] = 1
+            real_labels[i] = 1
+            true_p = true_p + 1
+        else:  # otherwise, this is a false negative
+            real_labels[i] = 2
+            false_n = false_n + 1
+
+    # set leftover estimated spikes to false positives
+    est_labels[est_labels == 0] = 3
+    false_p = len(est) - true_p
+
+    # get ER
+    ER = 1 - true_p/(true_p+false_p+false_n)
+
+    return ER, true_p, false_p, false_n, real_labels, est_labels
+
+
+
 def find_max_correlation_shifted(x, y):
     """Finding the maximum correlation between two arrays after shifting.
 
